b65437ff8531ecf89ca3353b069dcbb437301f8a
chazwilder <cwilder@chazwilder.onmicrosoft.com>
2024-05-23 11:58:38 -0400

    Adding Logger

---
+++ src/loggy.py
@@ -0,0 +1,13 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+logger = logging.getLogger('rag_logger')
+logger.setLevel(logging.DEBUG)
+
+handler = RotatingFileHandler('rag_logger.log', maxBytes=10*1024*1024, backupCount=1)
+handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
